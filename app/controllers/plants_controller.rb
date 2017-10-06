d74b01cdba90d73725d4971353f7a6b60b6a3c7d
Mariasole Alossa <mariasole.alossa@gmail.com>
2017-10-06 23:26:06 +0200

    Touched up update and destroy methods, and added their buttons to show page

--- app/controllers/plants_controller.rb
+++ app/controllers/plants_controller.rb
@@ -26,17 +26,19 @@ class PlantsController < ApplicationController
 
   def update
     @plant = Plant.find(params[:id])
-    @plant.update(params[:plant])
+    @plant.update(plant_params)
+    redirect_to plant_path(@plant)
   end
 
   def destroy
     @plant = Plant.find(params[:id])
     @plant.destroy
+    redirect_to plants_path
   end
 
   private
 
-  def pianta_params
+  def plant_params
     params.require(:plant).permit(:name, :photo, :description, :category, :esposizione, :fioritura, :altezza, :terreno, :densità, :vaso, :disponibile, :novità, :utile_per)
   end
 end
