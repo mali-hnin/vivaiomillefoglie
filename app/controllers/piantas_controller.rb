c13b401295c3c43c70ee29a9c22776a6b7e63269
Mariasole Alossa <mariasole.alossa@gmail.com>
2017-10-06 13:09:01 +0200

    fixing model issues

--- app/controllers/piantas_controller.rb
+++ app/controllers/piantas_controller.rb
@@ -8,11 +8,16 @@ class PiantasController < ApplicationController
   end
 
   def new
-
+    @pianta = Pianta.new
   end
 
   def create
-
+    @pianta = Pianta.new(pianta_params)
+    if @pianta.save
+      redirect_to pianta_path(@pianta)
+    else
+      render :new
+    end
   end
 
   def edit
@@ -25,5 +30,11 @@ class PiantasController < ApplicationController
 
   def destroy
 
+  end
+
+  private
+
+  def pianta_params
+    params.require(:pianta).permit(:name, :photo, :description, :category, :esposizione, :fioritura, :altezza, :terreno, :densità, :vaso, :disponibilità, :novità, :utile_per)
   end
 end
