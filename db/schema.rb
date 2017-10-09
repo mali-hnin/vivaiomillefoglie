3341500d98b0b46fac7537c38ed0706fa9b3c126
Mariasole Alossa <mariasole.alossa@gmail.com>
2017-10-09 19:37:58 +0200

    attachinary configured

--- db/schema.rb
+++ db/schema.rb
@@ -30,24 +30,6 @@ ActiveRecord::Schema.define(version: 20171009163644) do
     t.index ["attachinariable_type", "attachinariable_id", "scope"], name: "by_scoped_parent"
   end
 
-  create_table "pianta", force: :cascade do |t|
-    t.string "name"
-    t.string "photo"
-    t.text "description"
-    t.string "category"
-    t.string "esposizione"
-    t.string "fioritura"
-    t.string "altezza"
-    t.string "terreno"
-    t.string "densità"
-    t.string "vaso"
-    t.boolean "disponibilità"
-    t.boolean "novità"
-    t.string "utile_per"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "plants", force: :cascade do |t|
     t.string "name"
     t.string "photo"
