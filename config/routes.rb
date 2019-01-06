d11c9184d18e018d479e7fb6a4ecbf3d732a5e3b
Mariasole Alossa <mariasole.alossa@gmail.com>
2019-01-06 19:29:32 +0100

    adds flower lab page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   resources :plants
   get "/catalogo", to: "pages#catalogo"
   get "/giardini", to: "pages#giardini"
+  get "/millefoglie-flowers-lab", to: "pages#flowerslab", as: :flowerslab
 
   mount Attachinary::Engine => "/attachinary"
 end
