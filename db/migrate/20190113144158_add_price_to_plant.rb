01b46fb3ce667a4b033d7dc126c08548b4494fe4
Mariasole Alossa <mariasole.alossa@gmail.com>
2019-09-22 13:02:19 +0200

    but im in a hurry and the only person on this anyway

--- db/migrate/20190113144158_add_price_to_plant.rb
+++
@@ -1,5 +0,0 @@
-class AddPriceToPlant < ActiveRecord::Migration[5.2]
-  def change
-    add_monetize :plants, :price, amount: { null: true, default: nil }
-  end
-end
--- db/migrate/20190113153133_add_hide_to_plant.rb
+++
@@ -1,5 +0,0 @@
-class AddHideToPlant < ActiveRecord::Migration[5.2]
-  def change
-    add_column :plants, :nascondi, :boolean
-  end
-end
