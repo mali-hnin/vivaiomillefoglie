94ce272ddc18ed0174edc892768d4e22b470859b
Mariasole Alossa <mariasole.alossa@gmail.com>
2019-11-21 22:10:27 +0100

    mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,8 +91,8 @@ Rails.application.configure do
 
 
   config.action_mailer.delivery_method = :smtp
-  host = 'localhost:3000'
-  config.action_mailer.default_url_options = { :host => 'localhost:3000', protocol: 'http' }
+  host = 'https://floating-garden-71663.herokuapp.com'
+  config.action_mailer.default_url_options = { host: host }
 
   # SMTP settings for gmail
   config.action_mailer.smtp_settings = {
