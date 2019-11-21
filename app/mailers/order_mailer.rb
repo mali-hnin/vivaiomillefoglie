4a6d8dbb97d5552b36fb8f4a6afc92557c9cac2c
Mariasole Alossa <mariasole.alossa@gmail.com>
2019-11-21 21:57:07 +0100

    small mailer fixes

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -2,12 +2,12 @@ class OrderMailer < ApplicationMailer
   def new_order_email_admin
     @order = params[:order]
 
-    mail(to: ENV["ADMIN_EMAIL"], subject: "Hai ricevuto un nuovo ordine !")
+    mail(to: ENV["ADMIN_EMAIL"], subject: "NUOVO ORDINE vivaiomillefoglie.it!")
   end
 
   def new_order_email_customer
     @order = params[:order]
 
-    mail(to: @order.email, subject: "Il tuo ordine con Vivaio Millefoglie!")
+    mail(to: @order.email, subject: "Il tuo ordine con Vivaio Millefoglie 🍂")
   end
 end
