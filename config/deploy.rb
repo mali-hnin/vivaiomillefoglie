82f0e797b2f1d6e49b8d531e5960c8dde7ef5cdd
Mariasole Alossa <mariasole.alossa@gmail.com>
2020-09-13 17:20:58 +0200

    m

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,14 @@ lock "~> 3.14.1"
 set :application, "vivaiomillefoglie"
 set :repo_url, "git@github.com:mali-hnin/vivaiomillefoglie.git"
 
+# Deploy to the user's home directory
+set :deploy_to, "/home/deploy/#{fetch :application}"
+
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+
+# Only keep the last 5 releases to save disk space
+set :keep_releases, 5
+
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
 
