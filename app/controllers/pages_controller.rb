fe45f92a4eddecdeee586695140737838432952d
Mariasole Alossa <mariasole.alossa@gmail.com>
2021-01-30 19:19:58 +0100

    set up ig api and desktop grid with feed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,15 @@
+require 'uri'
+
 class PagesController < ApplicationController
   def home
   end
 
   def catalogo
+  end
+
+  def flowerslab
+    hash = IgService.new
+    @ig_photos = hash.get_ig_feed
+
   end
 end
---
+++ app/services/ig_service.rb
@@ -0,0 +1,10 @@
+require 'uri'
+class IgService
+
+  def get_ig_feed
+    response = HTTP.get("https://graph.instagram.com/me/media?fields=id,media_url,permalink,thumbnail_url,timestamp&access_token=#{ENV["IG_API_KEY"]}").to_s
+    parsed_response = JSON.parse(response)
+
+    parsed_response["data"]
+  end
+end
