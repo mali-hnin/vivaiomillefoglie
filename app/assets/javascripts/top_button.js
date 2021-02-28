951ff95b623bbf93cbf7ab5ac63c3fdc528eaa6d
Mariasole Alossa <mariasole.alossa@gmail.com>
2021-02-28 23:02:54 +0100

    breadcrumb

--- app/assets/javascripts/top_button.js
+++ app/assets/javascripts/top_button.js
@@ -5,7 +5,7 @@ mybutton = document.getElementById("topBtn");
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
     mybutton.style.display = "block";
   } else {
     mybutton.style.display = "none";
