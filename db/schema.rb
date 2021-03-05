2d6504541b94013c95a34b6e0df795617db9fa82
Mariasole Alossa <mariasole.alossa@gmail.com>
2021-03-05 20:00:04 +0100

    added provincia field

--- db/schema.rb
+++ db/schema.rb
@@ -2,15 +2,15 @@
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_192913) do
+ActiveRecord::Schema.define(version: 2021_03_05_185145) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,7 +40,7 @@ ActiveRecord::Schema.define(version: 2019_12_02_192913) do
     t.bigint "cart_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "quantity"
+    t.integer "quantity", default: 1
     t.index ["cart_id"], name: "index_line_items_on_cart_id"
     t.index ["plant_id"], name: "index_line_items_on_plant_id"
   end
@@ -62,6 +62,7 @@ ActiveRecord::Schema.define(version: 2019_12_02_192913) do
     t.string "periodo_spedizione"
     t.boolean "condizioni_di_vendita"
     t.boolean "trattamento_dati"
+    t.string "provincia"
     t.index ["cart_id"], name: "index_orders_on_cart_id"
   end
 
@@ -99,7 +100,7 @@ ActiveRecord::Schema.define(version: 2019_12_02_192913) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.boolean "admin"
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
