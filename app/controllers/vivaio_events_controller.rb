622b962d36e535207f864311a3d380b1ea8a71e0
Mariasole Alossa <mariasole.alossa@gmail.com>
2021-09-13 12:29:20 +0200

    filled events controller

--- app/controllers/vivaio_events_controller.rb
+++ app/controllers/vivaio_events_controller.rb
@@ -1,22 +1,40 @@
 class VivaioEventsController < ApplicationController
   def index
+    @vivaio_events = VivaioEvent.all
   end
 
   def new
+    @vivaio_event = VivaioEvent.new
   end
 
   def create
+    @vivaio_event = VivaioEvent.new(vivaio_event_params)
+    if @vivaio_event.save
+      redirect_to vivaio_event_path(@vivaio_event)
+    else
+      render :new
+    end
   end
 
   def show
+    @vivaio_event = VivaioEvent.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    @vivaio_event.update(vivaio_event_params)
+    redirect_to vivaio_event_path(@vivaio_event)
   end
 
   def destroy
+    @vivaio_event.destroy
+    redirect_to vivaio_events_path
+  end
+
+  private
+  def vivaio_event_params
+    params.require(:vivaio_event).permit(:title, :body, :content)
   end
 end
