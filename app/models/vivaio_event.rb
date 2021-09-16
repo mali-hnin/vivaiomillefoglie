5e65382230c9e1ded3622638dd1cb4b63dd0f4b2
Mariasole Alossa <mariasole.alossa@gmail.com>
2021-09-16 17:30:14 +0200

    added scope for past events

--- app/models/vivaio_event.rb
+++ app/models/vivaio_event.rb
@@ -2,5 +2,5 @@ class VivaioEvent < ApplicationRecord
   has_rich_text :content
   scope :by_event_date, -> { order("data_evento") }
   scope :future, -> { where("data_evento >= ? OR data_evento IS NULL = ?", Date.current, true) }
-  scope :tbd, -> { where("data_evento IS NULL = ?", true) }
+  scope :past, -> { where("data_evento < ?", Date.current) }
 end
