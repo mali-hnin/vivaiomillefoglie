22df06851377531628834edd4d08e3b19234cb5c
Mariasole Alossa <mariasole.alossa@gmail.com>
2021-09-24 20:44:39 +0200

    Revert "yarn"
    This reverts commit 653168b8fe4eb2fb0bd84a734b71d40236632037.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,15 +1,4 @@
 server '212.237.2.52', user: 'deploy', roles: %w{app db web}
-before "deploy:assets:precompile", "deploy:yarn_install"
-namespace :deploy do
-  desc "Run rake yarn install"
-  task :yarn_install do
-    on roles(:web) do
-      within release_path do
-        execute("cd #{release_path} && yarn install --silent --no-progress --no-audit --no-optional")
-      end
-    end
-  end
-end
 
 # server-based syntax
 # ======================
