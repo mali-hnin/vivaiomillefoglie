38dde478f4312cd86580f0941de47e44ff0cc0c8
Mariasole Alossa <mariasole.alossa@gmail.com>
2021-09-24 15:38:12 +0200

    trying 2

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_to, "/home/deploy/#{fetch :application}"
 
 set :deploy_via, :remote_cache
 
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads', 'node_modules'
 
 # Only keep the last 5 releases to save disk space
 set :keep_releases, 3
