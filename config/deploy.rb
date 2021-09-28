75b92ffc3237dae34cb6b5e51f1dc32e1d91a4b7
Mariasole Alossa <mariasole.alossa@gmail.com>
2021-09-28 14:25:35 +0200

    updated config

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,9 @@
 # config valid for current version and patch releases of Capistrano
-lock "~> 3.15.0"
+lock "~> 3.16.0"
 
 set :application, "vivaiomillefoglie"
 set :repo_url, "git@github.com:mali-hnin/vivaiomillefoglie.git"
+set :branch, ENV['BRANCH']
 
 # Deploy to the user's home directory
 set :deploy_to, "/home/deploy/#{fetch :application}"
