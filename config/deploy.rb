093fe247eed15028d1b639912ef16e0500bf83e1
Mariasole Alossa <mariasole.alossa@gmail.com>
2021-09-28 16:29:12 +0200

    new deploy config

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_to, "/home/deploy/#{fetch :application}"
 
 set :deploy_via, :remote_cache
 
-append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads', "public/packs", "node_modules"
 
 # Only keep the last 5 releases to save disk space
 set :keep_releases, 3
@@ -47,3 +47,15 @@ set :keep_releases, 3
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+before "deploy:assets:precompile", "deploy:yarn_install"
+
+namespace :deploy do
+  desc "Run rake yarn install"
+  task :yarn_install do
+    on roles(:web) do
+      within release_path do
+        execute("cd #{release_path} && yarn install --silent --no-progress --no-audit --no-optional")
+      end
+    end
+  end
+end
