19972ea0f1aec5222afe596bb47ced2d17ce09fa
Mariasole Alossa <mariasole.alossa@gmail.com>
2021-11-01 14:42:18 +0100

    refined past events scope

--- app/models/vivaio_event.rb
+++ app/models/vivaio_event.rb
@@ -1,6 +1,6 @@
 class VivaioEvent < ApplicationRecord
   has_rich_text :content
   scope :by_event_date, -> { order("data_evento") }
-  scope :future, -> { where("data_evento >= ? OR data_evento IS NULL = ?", Date.current, true) }
-  scope :past, -> { where("data_evento < ?", Date.current) }
+  scope :future, -> { where("data_evento >= ? OR data_fine_evento >= ? OR data_evento IS NULL = ?", Date.current, Date.current, true) }
+  scope :past, -> { where("data_fine_evento < ? OR data_evento < ?", Date.current, Date.current) }
 end
