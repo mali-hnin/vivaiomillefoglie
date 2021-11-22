dfef02503783653e340f25b770ee0de8cfb12c79
Mariasole Alossa <mariasole.alossa@gmail.com>
2021-11-22 19:57:25 +0100

    monile menu button transition

--- app/assets/javascripts/mobile_menu.js
+++ app/assets/javascripts/mobile_menu.js
@@ -1,3 +1,8 @@
 $("#mobileMenuBtn").click(function(){
   $("#mobileMenu").toggleClass("mobile-menu-closed");
+  if ($(this).html() == "☰") {
+    $(this).html("🗙");
+  } else if ($(this).html() == "🗙") {
+    $(this).html("☰");
+  }
 });
