c03c0fe881958a41a201d96a944e0039786ae726
Mariasole Alossa <mariasole.alossa@gmail.com>
2021-11-24 17:31:32 +0100

    dumb jquery is dumb

--- app/assets/javascripts/mobile_menu.js
+++ app/assets/javascripts/mobile_menu.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
     $("#mobileMenu").toggleClass("mobile-menu-closed");
     $("#modalBg").toggleClass("modal-opaque-bg");
     $("body").toggleClass("no-scroll");
-
     if ($("#mobileMenuBtn").html() == "<i class=\"fas fa-bars\"></i>") {
       $("#mobileMenuBtn").html("<i class=\"fas fa-times\"></i>");
     } else if ($("#mobileMenuBtn").html() == "<i class=\"fas fa-times\"></i>") {
