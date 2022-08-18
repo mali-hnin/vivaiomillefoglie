adeee682910460c5b7ec01a3bcb652bc798e1e58
Mariasole Alossa <mariasole.alossa@gmail.com>
2022-08-18 19:05:02 +0200

    LineItem tests

--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe LineItem, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  subject {
+    plant = Plant.create(name: "Achillea", price_cents: 5000)
+    cart = Cart.create
+    described_class.new(plant_id: plant.id, cart_id: cart.id, quantity: 1)
+  }
+
+  describe "create" do
+    it "should be valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+  end
+  describe "Associations" do
+    it { should belong_to(:plant) }
+    it { should belong_to(:cart) }
+  end
 end
