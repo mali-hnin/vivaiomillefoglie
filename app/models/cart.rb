14b47227491302bb67f4b5f23ec215dfb1469461
Mariasole Alossa <mariasole.alossa@gmail.com>
2022-08-19 15:18:10 +0200

    cart model tests

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,18 +1,18 @@
 class Cart < ApplicationRecord
   has_many :line_items, dependent: :destroy
-  has_many :order
+  has_one :order
   register_currency :eur
 
-    def add_plant(plant)
-      current_item = line_items.find_by(plant_id: plant.id)
+    # def add_plant(plant)
+    #   current_item = line_items.find_by(plant_id: plant.id)
 
-      if current_item
-        current_item.increment(:quantity)
-      else
-        current_item = line_items.build(plant_id: plant.id)
-      end
-      current_item
-    end
+    #   if current_item
+    #     current_item.increment(:quantity)
+    #   else
+    #     current_item = line_items.build(plant_id: plant.id)
+    #   end
+    #   current_item
+    # end
 
     def total_price
       line_items.to_a.sum { |item| item.total_price }
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,9 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  subject { Cart.new() }
+  subject {
+    described_class.new(id: 1900)
+  }
 
   it "is valid with valid attributes" do
     expect(subject).to be_valid
+  end
+
+  describe "Associations" do
+    it { should have_many(:line_items).dependent(:destroy) }
+    it { should have_one(:order) }
+
+  end
+
+  it "adds up line items prices for total bill" do
+    plant1 = Plant.create(id: 22, name: "Achillea", price_cents: 5000)
+    plant2 = Plant.create(id: 23, name: "Achillea", price_cents: 6000)
+    line_item1 = LineItem.create(plant_id: 22, cart_id: subject.id, quantity: 1)
+    line_item2 = LineItem.create(plant_id: 23, cart_id: subject.id, quantity: 1)
+    subject.line_items.append(line_item1)
+    subject.line_items.append(line_item2)
+    expect(subject.total_price).to eq(line_item1.total_price + line_item2.total_price)
   end
 end
