cfd9964b91ae28374d842a545fcae6a51eb6458b
Mariasole Alossa <mariasole.alossa@gmail.com>
2022-10-06 11:01:39 +0200

    changed es6 keyword to es5 because not supported

--- app/assets/javascripts/form_image_preview.js
+++ app/assets/javascripts/form_image_preview.js
@@ -1,5 +1,5 @@
 function preview() {
-  const input = document.getElementById('garden_photo')
+  var input = document.getElementById('garden_photo')
   document.getElementById('garden-preview').src = window.URL.createObjectURL(input.files[0])
   // frees memory --not strictly necessary bc it still gets flushed on page unload, but still better to have
   input.onload = function() {
