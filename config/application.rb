7fd4529b4584a8627c3b2f1eeb971da55408fcd7
Mariasole Alossa <mariasole.alossa@gmail.com>
2022-10-19 10:15:54 +0200

    readded capybara gem for tests

--- config/application.rb
+++ config/application.rb
@@ -27,7 +27,7 @@ module VivaioMillefoglie
     end
 
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 7.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
