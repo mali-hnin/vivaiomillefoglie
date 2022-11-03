251ae9ef97cb16bc6fc6ac956fd88b4b3a538970
Mariasole Alossa <mariasole.alossa@gmail.com>
2022-11-03 17:05:31 +0100

    updated to rails 7.0.4

--- spec/models/line_item_spec.rb
+++ spec/models/line_item_spec.rb
@@ -1,26 +1,27 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe LineItem, type: :model do
-
-  subject {
-    @plant = Plant.create(name: "Achillea", price_cents: 5000)
+  subject do
+    @plant = Plant.create(name: 'Achillea', price_cents: 5000)
     cart = Cart.create
     described_class.new(plant_id: @plant.id, cart_id: cart.id, quantity: 1)
-  }
+  end
 
-  describe "Validations" do
-    it "should be valid with valid attributes" do
+  describe 'Validations' do
+    it 'should be valid with valid attributes' do
       expect(subject).to be_valid
     end
   end
 
-  describe "Associations" do
+  describe 'Associations' do
     it { should belong_to(:plant) }
     it { should belong_to(:cart) }
   end
 
-  context "quantity is more than 1" do
-    it "adds price times quantity with total_price" do
+  context 'quantity is more than 1' do
+    it 'adds price times quantity with total_price' do
       subject.quantity = 2
       expect(subject.total_price).to eq(@plant.prezzo * subject.quantity)
     end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -1,40 +1,42 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Order, type: :model do
-  subject {
-    cart = Cart.create(id:1)
+  subject do
+    cart = Cart.create
     described_class.new(
       cart_id: cart.id,
-      name: "Jane Doe",
-      address: "aaa",
-      phone: 000000000000,
-      cf: 7689543456789,
-      citta: "Roma",
-      cap: 40987,
-      email: "miao@miao.com",
-      email_confirmation: "miao@miao.com",
-      metodo_pagamento: "Satuspay",
-      periodo_spedizione: "ottobre"
+      name: 'Jane Doe',
+      address: 'aaa',
+      phone: 0o00000000000,
+      cf: 7_689_543_456_789,
+      citta: 'Roma',
+      cap: 40_987,
+      email: 'miao@miao.com',
+      email_confirmation: 'miao@miao.com',
+      metodo_pagamento: 'Satuspay',
+      periodo_spedizione: 'ottobre'
     )
-  }
+  end
 
-  describe "Associations" do
+  describe 'Associations' do
     it { should belong_to(:cart) }
   end
 
-  describe "Validations" do
-    it { should validate_presence_of(:name)}
-    it { should validate_presence_of(:address)}
-    it { should validate_presence_of(:phone)}
-    it { should validate_presence_of(:cf)}
-    it { should validate_presence_of(:citta)}
-    it { should validate_presence_of(:cap)}
-    it { should validate_presence_of(:email)}
-    it { should validate_presence_of(:email_confirmation)}
-    it { should validate_presence_of(:metodo_pagamento)}
-    it { should validate_presence_of(:periodo_spedizione)}
-    it { should validate_confirmation_of(:email)}
-    it { should validate_acceptance_of(:trattamento_dati)}
-    it { should validate_acceptance_of(:condizioni_di_vendita)}
+  describe 'Validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:address) }
+    it { should validate_presence_of(:phone) }
+    it { should validate_presence_of(:cf) }
+    it { should validate_presence_of(:citta) }
+    it { should validate_presence_of(:cap) }
+    it { should validate_presence_of(:email) }
+    it { should validate_presence_of(:email_confirmation) }
+    it { should validate_presence_of(:metodo_pagamento) }
+    it { should validate_presence_of(:periodo_spedizione) }
+    it { should validate_confirmation_of(:email) }
+    it { should validate_acceptance_of(:trattamento_dati) }
+    it { should validate_acceptance_of(:condizioni_di_vendita) }
   end
 end
